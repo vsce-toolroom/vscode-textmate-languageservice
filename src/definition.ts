777883f8031cd8cde5644f2e9dc134926f1e984f
SNDST00M <sndst00m@outlook.com>
2023-01-24 18:52:51 +0000

    Hotfix for definition provider regressions

--- src/definition.ts
+++ src/definition.ts
@@ -7,11 +7,14 @@ import type { TextmateDocumentSymbolProvider } from './document-symbol';
 export class TextmateDefinitionProvider implements vscode.DefinitionProvider {
 	constructor(private _config: ConfigData, private _documentSymbolProvider: TextmateDocumentSymbolProvider) {}
 
-	private getComponentGlob(position: vscode.Position): string {
+	private getComponentGlob(position: vscode.Position): string | undefined {
 		const extensions = this._config.extensions;
 		if (!extensions) return;
 
-		const document = vscode.window.activeTextEditor.document;
+		const activeTextEditor = vscode.window.activeTextEditor;
+		if (!activeTextEditor) return;
+
+		const document = activeTextEditor.document;
 		const selection = document.getWordRangeAtPosition(position);
 		const componentName = document.getText(selection);
 		const extensionGlob = extensions.substring(1);
@@ -19,7 +22,10 @@ export class TextmateDefinitionProvider implements vscode.DefinitionProvider {
 	}
 
 	async getNestedPosition(position: vscode.Position): Promise<vscode.Position | undefined> {
-		const document = vscode.window.activeTextEditor.document;
+		const activeTextEditor = vscode.window.activeTextEditor;
+		if (!activeTextEditor) return;
+
+		const document = activeTextEditor.document;
 		const symbols = await this._documentSymbolProvider.provideDocumentSymbolInformation(document) as vscode.SymbolInformation[];
 		const selection = document.getWordRangeAtPosition(position);
 		const selectedText = document.getText(selection);
@@ -36,19 +42,21 @@ export class TextmateDefinitionProvider implements vscode.DefinitionProvider {
 		return;
 	}
 
-	async searchFiles(fileName: string | undefined): Promise<vscode.Uri[]> {
+	async searchFiles(fileName: string): Promise<vscode.Uri[]> {
 		return vscode.workspace.findFiles(fileName, this._config.exclude);
 	}
 
 	async provideDefinition(document: vscode.TextDocument, position: vscode.Position): Promise<vscode.Location[] | undefined> {
-		const locations: vscode.Location[] = [];
 		const filePosition = await this.getNestedPosition(position);
-		if (filePosition) locations.push(new vscode.Location(document.uri, filePosition));
+		if (filePosition) return [new vscode.Location(document.uri, filePosition)];
 
 		const componentGlob = this.getComponentGlob(position);
+		if (!componentGlob) return;
+
 		const workspaceUris = componentGlob ? await this.searchFiles(componentGlob) : [];
-		locations.push(...workspaceUris.map(fromUriToLocation));
-		return workspaceUris.length ? undefined : locations;
+		if (workspaceUris.length) return workspaceUris.map(fromUriToLocation);
+
+		return;
 	}
 }
 
