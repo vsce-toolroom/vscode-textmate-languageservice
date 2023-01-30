f5318fc2b7e6327738fbdccaf8ecdd293367b76a
SNDST00M <8972444-SNDST00M@users.noreply.gitlab.com>
2023-01-30 14:54:11 +0000

    [ci] Get pipeline to work in local Gitlab Runner

--- test/runner.ts
+++ test/runner.ts
@@ -1,17 +1,21 @@
 'use strict';
 
 import * as path from 'path';
-import { runTests as runElectronTests } from '@vscode/test-electron';
-// import { BrowserType, open, runTests as runBrowserTests } from '@vscode/test-web';
+import { runTests as runTestsInElectron } from '@vscode/test-electron';
+import { BrowserType, open, runTests as runTestsInBrowser } from '@vscode/test-web';
 
 async function main() {
 	try {
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../../..');
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+		const browserType: BrowserType = 'chromium';
+		const port = 8080;
+		const devTools = false;
+		const headless = true;
 		// Node environment.
-		await runElectronTests({
-			extensionDevelopmentPath,
+		await runTestsInElectron({
 			extensionTestsPath,
+			extensionDevelopmentPath,
 			launchArgs: [
 				'--disable-extensions',
 				'--disable-gpu',
@@ -21,14 +25,16 @@ async function main() {
 			]
 		});
 		// Web environment.
-		/**
-		const options = {
-			browserType: 'chromium' as BrowserType,
-			extensionDevelopmentPath
-		};
-		await open(options);
-		await runBrowserTests({ ...options, extensionTestsPath });
-		 */
+		/***
+		await runTestsInBrowser({
+			extensionTestsPath,
+			extensionDevelopmentPath,
+			browserType,
+			port,
+			devTools,
+			headless
+		});
+		 ***/
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
