81ffb5c183e0989c064840406f52682e8033c8d9
SNDST00M <sndst00m@outlook.com>
2023-02-08 22:13:17 +0000

    Rewire test suite env from Mocha to Jest-Webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+/** @type {webpack.Configuration} */
+module.exports = {
+	mode: 'none',
+	target: 'node',
+	entry: {
+		index: './src/index.ts',
+		'runner.test': './test/runner.test.ts',
+		'suite.test': './test/suite.test.ts'
+	},
+	externals: {
+		'vscode': 'commonjs vscode',
+		'crypto': 'commonjs crypto',
+		'fs': 'commonjs fs',
+		'@vscode/test-electron': 'commonjs @vscode/test-electron',
+		'@vscode/test-web': 'commonjs @vscode/test-electron'
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	module: {
+		rules: [
+			{ test: /\.ts$/, loader: 'ts-loader' }
+		]
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.join(__dirname, 'node_modules', 'vscode-oniguruma', 'release', 'onig.wasm'),
+					to: path.resolve(__dirname, 'dist')
+				}
+			]
+		})
+	]
+};
