86490c0a74abc3afd467217dc4c5cc96c89f1f69
SNDST00M <sndst00m@outlook.com>
2023-02-22 13:29:50 +0000

    [nit] Typo in comment

--- src/util/oniguruma.ts
+++ src/util/oniguruma.ts
@@ -7,7 +7,7 @@
 import * as vscodeOniguruma from 'vscode-oniguruma';
 import type * as vscodeTextmate from 'vscode-textmate';
 
-// Use webpack + encoded-uint8array-loader to generate an `Uint8Array` WASM module.
+// Use webpack + encoded-uint8array-loader to generate a `Uint8Array` WASM module.
 // This is not streaming :[ but vscode libs must bundle WASM deps to support web ecosystem.
 // Better alternative is using copy-webpack-plugin + fetch to include the WASM file.
 import * as data from '../../node_modules/vscode-oniguruma/release/onig.wasm';
