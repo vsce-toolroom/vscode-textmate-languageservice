afced29472798b5e6bf2d334224669b0d18eab17
SNDST00M <sndst00m@outlook.com>
2023-02-22 10:02:23 +0000

    Remove support for JSONC

--- src/util/loader.ts
+++ src/util/loader.ts
@@ -3,15 +3,14 @@
 import * as vscode from 'vscode';
 import type { JsonValue, PartialDeep } from 'type-fest';
 
+// We assume that the document language is in UTF-8.
 const decoder = new TextDecoder('utf-8');
-const jsonCommentsRegex = /\/\*[\s\S]*?\*\/|\/\/.*/g;
 
 export async function readFileText(uri: vscode.Uri): Promise<string> {
-	// We assume that the document language is in UTF-8
 	try {
 		return decoder.decode(await vscode.workspace.fs.readFile(uri));
 	} catch (e) {
-		throw e;
+		throw e as vscode.FileSystemError;
 	}
 }
 
@@ -19,8 +18,7 @@ type PartialJsonValue = PartialDeep<JsonValue>;
 
 export async function loadJsonFile<T = PartialJsonValue>(uri: vscode.Uri): Promise<T> {
 	try {
-		let text = await readFileText(uri);
-		text = text.replace(jsonCommentsRegex, ''); // support jsonc!
+		const text = await readFileText(uri);
 		return JSON.parse(text) as T;
 	} catch (e) {
 		if (e && typeof (e as Error).stack === 'string') {
