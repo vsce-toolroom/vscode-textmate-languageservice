e754824059c2818205495d5a44b7dd401f9acf6c
zm-cttae <114668551+zm-cttae@users.noreply.github.com>
2023-09-02 15:26:31 +0100

    Lint & extend build test env staging scripts

--- scripts/reset.js
+++ scripts/reset.js
@@ -1,9 +1,16 @@
 'use strict';
 
 const shelljs = require('shelljs');
+const path = require('path');
 
 const extensionPath = process.argv[2];
 const extensionTestDataFolders = ['data', 'samples'];
 
-shelljs.rm("-rf", ...extensionTestDataFolders.map(f => `${extensionPath}/${f}`));
-shelljs.rm("-rf", "*.tgz", `${extensionPath}/*.tgz`);
+const root = path.relative(__dirname, '..');
+
+shelljs.cd(extensionPath);
+shelljs.exec('npm un vscode-textmate-languageservice');
+shelljs.cd(root);
+
+shelljs.rm('-rf', ...extensionTestDataFolders.map(f => `${extensionPath}/${f}`));
+shelljs.rm('-rf', "*.tgz", `${extensionPath}/*.tgz`);
