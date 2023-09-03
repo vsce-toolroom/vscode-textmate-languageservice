fabc73c8ca1fe319fbb601078db05c221fa82c0b
zm-cttae <114668551+zm-cttae@users.noreply.github.com>
2023-09-03 12:42:00 +0100

    WIP for testing API language methods

---
+++ test/suite/api/window.test.ts
@@ -0,0 +1,100 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+import { strictEqual } from '../../util/assert';
+import TextmateLanguageService from '../../../src/main';
+import { textmateService, documentServicePromise, tokenServicePromise } from '../../util/factory';
+import { BASENAMES, getSampleFileUri } from '../../util/files';
+import { TextmateScopeSelector } from '../../util/common';
+
+const { getScopeInformationAtPosition, getScopeRangeAtPosition, getTokenInformationAtPosition } = TextmateLanguageService.api;
+const resolver = textmateService.resolver;
+
+let titleData: Awaited<ReturnType<typeof getTitleData>>;
+
+type TextmateScopeSelectorType = typeof TextmateScopeSelector.prototype;
+
+const languageScopeMap: Record<string, string> = {
+	'mediawiki': 'string.quoted.other.heading.mediawiki',
+	'matlab': 'entity.name.type.class.matlab',
+	'typescript': 'entity.name.type.class.ts',
+};
+
+const languageSelectorMap: Record<string, TextmateScopeSelectorType> =
+	Object.fromEntries(
+		Object.entries(languageScopeMap)
+		.map(([k, v]) => [k, new TextmateScopeSelector(v)])
+	);
+
+const languageStandardTypeMap = {
+	'mediawiki': vscode.StandardTokenType.String,
+	'matlab': vscode.StandardTokenType.Other,
+	'typescript': vscode.StandardTokenType.Other,
+};
+
+suite('test/suite/tokenizer.test.ts - TokenizerService class (src/services/tokenizer.ts)', async function() {
+	this.timeout(5000);
+
+	this.beforeAll(async function() {
+		titleData = await getTitleData();
+	})
+
+	test('getScopeInformationAtPosition(): Promise<TextmateToken>', async function() {
+		vscode.window.showInformationMessage('API `getScopeInformationAtPosition` method (src/api.ts)');
+
+		const scopeInformation = await getScopeInformationAtPosition(titleData.document, titleData.position);
+
+		strictEqual(scopeInformation.line, 0);
+
+		strictEqual(scopeInformation.startIndex, titleData.token.startIndex);
+		strictEqual(scopeInformation.endIndex, titleData.token.endIndex);
+
+		strictEqual(scopeInformation.level, 0);
+
+		strictEqual(scopeInformation.text, titleData.basename);
+
+		const scopeType = languageScopeMap[globalThis.languageId];
+		strictEqual(scopeInformation.type, scopeType);			
+	});
+
+	test('getScopeRangeAtPosition(): Promise<TextmateToken>', async function() {
+		vscode.window.showInformationMessage('API `getScopeRangeAtPosition` method (src/api.ts)');
+
+		const scopeRange = await getScopeRangeAtPosition(titleData.document, titleData.position);
+		strictEqual(scopeRange.isEqual(titleData.range), true);
+	});
+
+	test('getScopeInformationAtPosition(): Promise<TextmateToken>', async function() {
+		vscode.window.showInformationMessage('API `getScopeInformationAtPosition` method (src/api.ts)');
+
+		const tokenInformation = await getTokenInformationAtPosition(titleData.document, titleData.position);
+		strictEqual(tokenInformation.range.isEqual(titleData.range), true);
+
+		const standardType = languageStandardTypeMap[globalThis.languageId];
+		strictEqual(tokenInformation.type, standardType);	
+	});
+});
+
+async function getTitleData() {
+	const documentService = await documentServicePromise;
+	const tokenService = await tokenServicePromise;
+
+	const basename = BASENAMES[globalThis.languageId][0];
+	const resource = getSampleFileUri(basename);
+
+	const document = await documentService.getDocument(resource);
+	const tokens = await tokenService.fetch(document);
+
+	const selector = languageSelectorMap[basename];
+	const token = tokens.find(t => t.line === 0 && selector.match(t.scopes));
+
+	if (!token) {
+		throw new Error('no title symbol present on first line');
+	}
+
+	const position = new vscode.Position(token.line, token.startIndex);
+	const range = new vscode.Range(token.line, token.startIndex, token.line, token.endIndex);
+
+	return { basename, document, position, range, token };
+}
