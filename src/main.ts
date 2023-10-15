e3ed98db555176feba10099010f9474844ab83ce
zm-cttae <114668551+zm-cttae@users.noreply.github.com>
2023-10-15 16:21:08 +0100

    Patch ctor for `plaintext` service generator

--- src/main.ts
+++ src/main.ts
@@ -69,7 +69,7 @@ export default class TextmateLanguageService {
 		const extension = context?.extension || resolver.getExtensionFromLanguageId(languageId);
 		const manifest = extension?.packageJSON as ExtensionManifest | undefined;
 
-		if (!manifest) {
+		if (languageId !== 'plaintext' && !manifest) {
 			throw new Error('could not find extension contributing language ID "' + languageId + '"');
 		}
 
@@ -78,14 +78,19 @@ export default class TextmateLanguageService {
 		const grammarData = resolver.getGrammarDefinitionFromLanguageId(languageId);
 		this[_private].grammarPromise = registry.loadGrammar(grammarData.scopeName);
 
-		const paths = manifest['textmate-languageservices'] || {};
-		const filepath = paths[languageId] || './textmate-configuration.json';
-
-		const uri = vscode.Uri.joinPath(extension.extensionUri, filepath);
 		const languageData = resolver.getLanguageDefinitionFromId(languageId);
-		this[_private].configPromise = loadJsonFile<ConfigJson>(uri, '{}')
-			.then(json => new ConfigData(json, languageData))
-			.catch(() => new ConfigData({}, languageData));
+
+		if (languageId !== 'plaintext') {
+			const paths = manifest['textmate-languageservices'] || {};
+			const filepath = paths[languageId] || './textmate-configuration.json';
+
+			const uri = vscode.Uri.joinPath(extension.extensionUri, filepath);
+			this[_private].configPromise = loadJsonFile<ConfigJson>(uri, '{}')
+				.then(json => new ConfigData(json, languageData))
+				.catch(() => new ConfigData({}, languageData));
+		} else {
+			this[_private].configPromise = Promise.resolve(new ConfigData({}, languageData));
+		}
 	}
 
 	public async initTokenService(): Promise<TokenizerService> {
