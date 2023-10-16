3f0da9576a8ddfebaf6d7bb9c517245e845d259b
zm-cttae <114668551+zm-cttae@users.noreply.github.com>
2023-10-16 19:14:25 +0100

    Enable configuration tests for plaintext

--- test/suite/api/language-configuration.test.ts
+++ test/suite/api/language-configuration.test.ts
@@ -10,68 +10,58 @@ import TextmateLanguageService from '../../../src/main';
 const { getGrammarConfiguration, getLanguageConfiguration, getContributorExtension } = TextmateLanguageService.api;
 
 const languageExtensionMap: Record<string, string> = {
-	'mediawiki': '.mediawiki',
+	'mediawiki': '.txt',
 	'matlab': '.m',
 	'typescript': '.ts',
 };
 
 const languageScopeNameMap: Record<string, string> = {
-	'mediawiki': 'text.html.mediawiki',
+	'mediawiki': 'text',
 	'matlab': 'source.matlab',
 	'typescript': 'source.ts',
 };
 
 const languageContributorMap: Record<string, string> = {
-	'mediawiki': 'sndst00m.mediawiki',
+	'mediawiki': void 0,
 	'matlab': 'Gimly81.matlab',
 	'typescript': 'vscode.typescript'
 }
+
+const languageId = globalThis.languageId === 'mediawiki' ? 'plaintext' : globalThis.languageId;
 
 suite('test/api/languageConfiguration.test.ts (src/api.ts)', async function() {
 	this.timeout(5000);
 
 	test('getLanguageConfiguration(): LanguageDefinition', async function() {
-		if (globalThis.languageId === 'mediawiki') {
-			this.skip();
-		}
-
 		vscode.window.showInformationMessage('API `getScopeInformationAtPosition` method (src/api.ts)');
 
 		const languageConfiguration = getLanguageConfiguration(globalThis.languageId);
 
-		strictEqual(languageConfiguration.id, globalThis.languageId);
+		strictEqual(languageConfiguration.id, languageId);
 
 		const languageFileExtension = languageExtensionMap[globalThis.languageId];
 		strictEqual(languageConfiguration.extensions?.includes(languageFileExtension), true);
 	});
 
 	test('getGrammarConfiguration(): GrammarLanguageDefinition', async function() {
-		if (globalThis.languageId === 'mediawiki') {
-			this.skip();
-		}
-
 		vscode.window.showInformationMessage('API `getScopeInformationAtPosition` method (src/api.ts)');
 
 		const grammarConfiguration = getGrammarConfiguration(globalThis.languageId);
 
-		strictEqual(grammarConfiguration.language, globalThis.languageId);
+		strictEqual(grammarConfiguration.language, languageId);
 
 		const languageScopeName = languageScopeNameMap[globalThis.languageId]
 		strictEqual(grammarConfiguration.scopeName, languageScopeName);
 	});
 
 	test('getContributorExtension(): vscode.Extension | void', async function () {
-		if (globalThis.languageId === 'mediawiki') {
-			this.skip();
-		}
-
 		vscode.window.showInformationMessage('API `getContributorExtension` method (src/api.ts)');
 
 		const extension = getContributorExtension(globalThis.languageId);
 
-		strictEqual(typeof extension === 'object', true);
+		strictEqual(typeof extension === 'object', globalThis.languageId !== 'mediawiki');
 
 		const languageContributorId = languageContributorMap[globalThis.languageId];
-		strictEqual((extension as vscode.Extension<unknown>).id, languageContributorId);
+		strictEqual((extension || {}).id, languageContributorId);
 	});
 });
