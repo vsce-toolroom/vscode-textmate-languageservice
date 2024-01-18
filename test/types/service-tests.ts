6c1359133a1ffce77b0e658bfa3ca4b62d20423a
zm-cttae <114668551+zm-cttae@users.noreply.github.com>
2024-01-18 23:36:19 +0000

    Implement type regression testing

---
+++ test/types/service-tests.ts
@@ -0,0 +1,27 @@
+import * as vscode from 'vscode';
+import TextmateLanguageService from '../..';
+
+const TextmateScopeSelector = TextmateLanguageService.utils.TextmateScopeSelector;
+
+// $ExpectType TextmateScopeSelector
+const commentScopeSelector = new TextmateScopeSelector('comment');
+
+// $ExpectType boolean
+const result = commentScopeSelector.match(['source.ts', 'comment.line.double-slash.ts']);
+if (result === true) {
+	void vscode.window.showInformationMessage('TS comment matching OK');
+}
+
+// $ExpectType TextmateLanguageService
+const typescriptService = new TextmateLanguageService('typescript');
+
+// $ExpectType TokenizerService
+const textmateTokenService = await typescriptService.initTokenService();
+
+const textDocument = vscode.window.activeTextEditor.document;
+// $ExpectType TextmateToken[]
+const tokens = await textmateTokenService.fetch(textDocument);
+
+if (tokens[tokens.length - 1].line === (textDocument.lineCount - 1)) {
+	void vscode.window.showInformationMessage('Tokenization OK');
+}
